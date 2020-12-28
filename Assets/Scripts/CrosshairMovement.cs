3d85e76246a205423c378e6e81c04f444ddd5f3e
Hank <shiawhern@gmail.com>
2020-12-27 20:59:07 -0800

    CrossHair Movement

--- Assets/Scripts/CrosshairMovement.cs
+++ Assets/Scripts/CrosshairMovement.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class CrosshairMovement : MonoBehaviour
 {
+    [SerializeField]
+    float moveSpeed;
+    Transform crosshair;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        crosshair = this.gameObject.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float verticalMove = Input.GetAxisRaw("Mouse Y");
+        float horizontalMove = Input.GetAxisRaw("Mouse X");
+
+        crosshair.position += new Vector3(horizontalMove, verticalMove).normalized * moveSpeed * Time.deltaTime;
     }
 }
