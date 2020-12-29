b5902e438e3a9a3987261db4d2f56f9056d266e5
Hank <shiawhern@gmail.com>
2020-12-28 19:40:11 -0800

    lifetime script

---
+++ Assets/Scripts/Lifetime.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Lifetime : MonoBehaviour
+{
+    [SerializeField]
+    float seconds = 10;
+    [SerializeField]
+    float fadeTrigger = 0.5f; // when to begin fading effect as a ratio of total lifetime
+
+    float elapsedTime = 0;
+    SpriteRenderer spriteRenderer;
+    Color currentColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime >= seconds * fadeTrigger)   // If fade effect has begun..
+        {
+            updateVisibility(); // change the sprite's alpha value
+        }
+
+        if (elapsedTime >= seconds) // end of life reached
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void updateVisibility()
+    {
+        Color newColor = spriteRenderer.color;
+        newColor.a = 1 - (elapsedTime - seconds * fadeTrigger) / (seconds * fadeTrigger); // currently linearly begins to disappear after trigger ratio is passed
+        spriteRenderer.color = newColor;
+    }
+}
